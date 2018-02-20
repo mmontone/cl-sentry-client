ee9e26bb5610a6c2f98c3cdf40dd0ebe7d701a99
Mariano Montone <marianomontone@gmail.com>
2018-02-20 11:02:49 -0300

    Client testing function

--- package.lisp
+++ package.lisp
@@ -1,6 +1,7 @@
 (defpackage #:sentry-client
   (:use #:cl)
   (:export #:initialize-sentry-client
+           #:test-sentry-client
            #:capture-exception
            #:capture-message
            #:with-sentry-error-handler))
--- sentry-client.lisp
+++ sentry-client.lisp
@@ -131,14 +131,7 @@
      (error (e)
        (sentry-client:capture-exception e))))
 
-(defvar *unix-epoch-difference*
-  (encode-universal-time 0 0 0 1 1 1970 0))
-
-(defun universal-to-unix-time (universal-time)
-  (- universal-time *unix-epoch-difference*))
-
-(defun unix-to-universal-time (unix-time)
-  (+ unix-time *unix-epoch-difference*))
-
-(defun get-unix-time ()
-  (universal-to-unix-time (get-universal-time)))
+(defun test-sentry-client (&optional (sentry-client *sentry-client*))
+  (handler-case (error "Sentry client test")
+    (error (e)
+      (capture-exception e))))
