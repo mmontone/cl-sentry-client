e3f95fa294c97c42e6e13538eee803dc547b7f0c
Mariano Montone <marianomontone@gmail.com>
2022-08-17 14:33:05 -0300

    Async client

--- async.lisp
+++ async.lisp
@@ -15,20 +15,24 @@
     (setf *tasks-runner* (make-instance 'simple-tasks:queued-runner))
     (setf *tasks-thread* (simple-tasks:make-runner-thread *tasks-runner*))))
 
-(defun initialize-async-sentry-client (dsn &rest args &key tags)
+(defun initialize-async-sentry-client (dsn &rest args)
   "Initialize an async sentry client instance."
   (setf *sentry-client*
-        (make-instance 'async-sentry-client
-                       :dsn (read-dsn dsn)
-                       :tags tags)))
+        (apply #'make-instance 'async-sentry-client
+               :dsn (read-dsn dsn)
+               args)))
 
 (defmethod initialize-instance :after ((sentry-client async-sentry-client) &rest initargs)
   (declare (ignore initargs))
   ;; Start background tasks for async client
   (maybe-initialize-tasks))
 
-(defmethod client-capture-exception ((sentry-client async-sentry-client) condition &rest args &key tags)
-  (let ((json (encode-exception-event condition sentry-client)))
+(defmethod client-capture-exception ((sentry-client async-sentry-client) condition &key extras transaction)
+  (let ((json (encode-exception-event condition
+                                      :sentry-client sentry-client
+                                      :extras extras
+                                      :transaction transaction)))
     (let ((task (make-instance 'simple-tasks:call-task
-                               :func (lambda () (post-sentry-request json sentry-client)))))
+                               :func (lambda ()
+                                       (post-sentry-request json sentry-client)))))
       (simple-tasks:schedule-task task *tasks-runner*))))
